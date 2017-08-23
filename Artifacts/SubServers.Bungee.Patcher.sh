1dfdc494f30568ba788de2b98388b9ff68614b24
ME1312 <admin@me1312.net>
2017-08-23 10:48:42 -0400

    Better Patch Logging

--- Artifacts/SubServers.Bungee.Patcher.sh
+++ Artifacts/SubServers.Bungee.Patcher.sh
@@ -27,33 +27,36 @@ if [ -d "Buildtools" ]; then
 fi
 mkdir BuildTools
 cd BuildTools
-echo Extracting $1...
-jar xf ../$1; retvala=$?;
+echo ">> Extracting $1..."
+jar xvf ../$1; retvala=$?;
 if [ $retvala -eq 0 ]
   then
-    echo Extracting $2...
-    jar xf ../$2; retvalb=$?;
+    echo ">> Extracting $2..."
+    jar xvf ../$2; retvalb=$?;
 	if [ $retvalb -eq 0 ]
 	  then
-		echo Recompiling...
+		if [ -d "../SubServers.Patched.jar" ]; then
+			rm -Rf ../SubServers.Patched.jar
+		fi
+		echo ">> Recompiling..."
 		jar cvfm ../SubServers.Patched.jar META-INF/MANIFEST.MF .; retvalc=$?;
 		if [ $retvalc -eq 0 ]
 		  then
-			echo Cleaning Up...
+			echo ">> Cleaning Up..."
 			rm -Rf ../Buildtools
 			exit 0;
 		else
-			echo Error Recomiling Files
+			echo ">> Error Recomiling Files"
 			rm -Rf ../Buildtools
 			exit 4
 		fi
 	else
-		echo Error Decompiling $2 Files
+		echo ">> Error Decompiling $2 Files"
 		rm -Rf ../Buildtools
 		exit 3
 	fi
 else
-    echo Error Decompiling $1 Files
+    echo ">> Error Decompiling $1 Files"
     rm -Rf ../Buildtools
 	exit 3
 fi
