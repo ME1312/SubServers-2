6925a7bccb9bfedfa6bda20b52e9d69fa6671f59
ME1312 <admin@me1312.net>
2018-10-22 20:06:39 -0400

    Fix version inconsistency

--- SubServers.Sync/src/net/ME1312/SubServers/Sync/SubPlugin.java
+++ SubServers.Sync/src/net/ME1312/SubServers/Sync/SubPlugin.java
@@ -44,7 +44,7 @@ public final class SubPlugin extends BungeeCord implements Listener {
     public boolean redis = false;
     public final SubAPI api = new SubAPI(this);
     public SubDataClient subdata = null;
-    public static final Version version = Version.fromString("2.13.1b");
+    public static final Version version = Version.fromString("2.13.2a");
 
     public final boolean isPatched;
     public long lastReload = -1;
